7be91a4665edf68bb2b34991d829fa17823ec6ec
Gavrilchenko_Stepan <atom47228@gmail.com>
2025-12-18 22:55:37 +0300

    Добавил интерфейс IMainView для главной формы

---
+++ Directory_of_products_and_components/MyLibrary/IMainView.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public interface IMainView
+    {
+        string SearchText { get; set; }
+        Product SelectedProduct { get; }
+
+        event EventHandler LoadEvent;
+        event EventHandler SearchEvent;
+        event EventHandler AddProductEvent;
+        event EventHandler EditProductEvent;
+        event EventHandler DeleteProductEvent;
+        event EventHandler ViewDetailsEvent;
+        event EventHandler WhereUsedEvent;
+        event EventHandler RefreshEvent;
+
+        void Show();
+        void Close();
+        void DisplayProducts(List<Product> products);
+        void DisplayProductInfo(ProductComposition composition);
+        void ShowMessage(string message, string title);
+        bool ConfirmDelete(string message, string title);
+        void ClearProductInfo();
+        void ShowProductForm(Product product = null);
+        void ShowProductDetailForm(ProductComposition composition);
+        void ShowWhereUsedForm(List<ProductComposition> compositions);
+    }
+}
