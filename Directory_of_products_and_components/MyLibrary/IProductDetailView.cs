a7cd5dc3532e8e8df35d128672134bf6023fa584
Gavrilchenko_Stepan <atom47228@gmail.com>
2025-12-18 22:56:54 +0300

    Добавил интерфейс IProductDetailView для формы деталей изделия

---
+++ Directory_of_products_and_components/MyLibrary/IProductDetailView.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public interface IProductDetailView
+    {
+        ProductComposition Composition { get; set; }
+        CompositionItem SelectedComponent { get; }
+
+        event EventHandler LoadEvent;
+        event EventHandler AddComponentEvent;
+        event EventHandler EditComponentEvent;
+        event EventHandler RemoveComponentEvent;
+        event EventHandler SaveEvent;
+        event EventHandler CloseEvent;
+        event EventHandler RefreshEvent;
+
+        void Show();
+        void Close();
+        void DisplayComposition(ProductComposition composition);
+        void ShowMessage(string message, string title);
+        void ShowComponentSelectionForm(List<Component> availableComponents);
+        void ShowQuantityForm(CompositionItem component, int currentQuantity);
+        bool ConfirmRemove(string message, string title);
+        void RefreshComposition();
+    }
+}
