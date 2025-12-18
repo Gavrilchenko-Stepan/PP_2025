4672c32ab4f435af9541d0b6b06ec2a1d4df67ef
Gavrilchenko_Stepan <atom47228@gmail.com>
2025-12-18 22:00:42 +0300

    Модель ProductComponent для связи изделий и комплектующих

---
+++ Directory_of_products_and_components/MyLibrary/ProductComponent.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MyLibrary
+{
+    public class ProductComponent
+    {
+        public int ProductId { get; set; }
+        public int ComponentId { get; set; }
+        public int Quantity { get; set; }
+    }
+}
